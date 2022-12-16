caee05fada2c9e6734047ad01607eb95a1b939b5
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-16 22:18:14 +0200

    Added_User_model

---
+++ Back_End/Src/models/users.model.ts
@@ -0,0 +1,13 @@
+import db from "../database";
+import UserType from "../types/User.type";
+
+// export class User {
+//     async index() {
+//         try {
+//         } catch (error) {}
+//     }
+//     async show();
+//     async create();
+//     async update();
+//     async delete();
+// }
