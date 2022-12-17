ae4d636825c599520bd8c53eb83ed1fc09f86fe3
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-17 20:34:46 +0200

    Added_users_Models

--- Back_End/Src/models/users.model.ts
+++ Back_End/Src/models/users.model.ts
@@ -1,13 +1,20 @@
 import db from "../database";
 import UserType from "../types/User.type";
 
-// export class User {
-//     async index() {
-//         try {
-//         } catch (error) {}
-//     }
-//     async show();
-//     async create();
-//     async update();
-//     async delete();
-// }
+export class User {
+    async index() {
+        try {
+            const connection = await db.connect();
+            const sql = `SELECT * FROM users;`;
+            const query = await connection.query(sql);
+            connection.release();
+            return query.rows;
+        } catch (error) {
+            throw new Error("Cant Retreve Data From Table Users");
+        }
+    }
+    // async show();
+    // async create();
+    // async update();
+    // async delete();
+}
