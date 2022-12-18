13d8be9b6df4b5cb2643f417dec5f8dc6fa684c4
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-18 22:52:05 +0200

    added_test_for_db

--- Back_End/Src/index.ts
+++ Back_End/Src/index.ts
@@ -1,15 +1,28 @@
 import express, { Request, Response } from "express";
+import routes from "./routes/index.routes";
+import errorMiddleware from "./middleware/error.middleware";
 import dotenv from "dotenv";
+import db from "./database";
 
 dotenv.config();
 const { SERVER_PORT } = process.env;
 
 const app = express();
+app.use("/api", routes);
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("HOME PAGE");
+    throw new Error();
 });
+const test = async () => {
+    const connection = await db.connect();
+    const sql = "select * from users";
+    const result = await connection.query(sql);
+    console.log(result.rows);
+    connection.release();
+};
+test();
 
+app.use(errorMiddleware);
 app.listen(SERVER_PORT, () => {
     console.log(`SERVER IS RUNNING ON PORT ${SERVER_PORT}`);
 });
