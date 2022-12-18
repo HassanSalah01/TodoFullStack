3befffa82aab73a30e25a762473c90d21850cb44
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-18 22:52:47 +0200

    Updated_user_Model

--- Back_End/Src/models/users.model.ts
+++ Back_End/Src/models/users.model.ts
@@ -2,7 +2,7 @@ import db from "../database";
 import UserType from "../types/User.type";
 
 export class User {
-    async index() {
+    async index(): Promise<UserType[]> {
         try {
             const connection = await db.connect();
             const sql = `SELECT * FROM users;`;
@@ -13,8 +13,32 @@ export class User {
             throw new Error("Cant Retreve Data From Table Users");
         }
     }
-    // async show();
-    // async create();
+    async show(id: number): Promise<UserType> {
+        try {
+            const connection = await db.connect();
+            const sql = `SELECT * FROM users WHERE id = ($1) ;`;
+            const query = await connection.query(sql, [id]);
+            connection.release();
+            return query.rows[0];
+        } catch (error) {
+            throw new Error("Cant Retreve Data From Table Users");
+        }
+    }
+    async create(u: UserType): Promise<UserType> {
+        try {
+            const connection = await db.connect();
+            const sql = `INSERT INTO users (email,username,password) VALUES ($1,$2,$3) RETURNING *;`;
+            const query = await connection.query(sql, [
+                u.email,
+                u.username,
+                u.password,
+            ]);
+            connection.release();
+            return query.rows[0];
+        } catch (error) {
+            throw new Error("Cant Retreve Data From Table Users");
+        }
+    }
     // async update();
     // async delete();
 }
