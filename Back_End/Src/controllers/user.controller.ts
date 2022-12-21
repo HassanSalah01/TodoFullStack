ff79fdcb231297b6648693ec18dbc2c1ed8dbe6a
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-21 23:06:49 +0200

    updated_user_controllers

--- Back_End/Src/controllers/user.controller.ts
+++ Back_End/Src/controllers/user.controller.ts
@@ -22,3 +22,41 @@ export const show = async (req: Request, res: Response) => {
     }
 };
 
+export const create = async (req: Request, res: Response) => {
+    try {
+        const u = {
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+        };
+        const result = await user.create(u);
+        res.send(result);
+    } catch (error) {
+        res.status(400);
+        throw new Error(`${error}`);
+    }
+};
+
+export const update = async (req: Request, res: Response) => {
+    try {
+        const u = {
+            id: parseInt(req.params.id),
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+        };
+        const result = await user.update(u);
+        res.send(result);
+    } catch (error) {
+        throw new Error(`${error}`);
+    }
+};
+
+export const del = async (req: Request, res: Response) => {
+    try {
+        const result = await user.delete(parseInt(req.params.id));
+        res.send(result);
+    } catch (error) {
+        throw new Error(`${error}`);
+    }
+};
