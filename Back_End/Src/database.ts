43387a2dc8750a8d30fd3bab0bb5ce14474d3e14
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-22 21:39:22 +0200

    Updated_Database.ts

--- Back_End/Src/database.ts
+++ Back_End/Src/database.ts
@@ -5,26 +5,30 @@ dotenv.config();
 
 const {
     POSTGRES_USER,
-    POSTGRES_DB_DEV,
+    POSTGRES_DEV_DB,
     POSTGRES_DB_TEST,
     POSTGRES_HOST,
     POSTGRES_PASSWORD,
     ENV,
 } = process.env;
 
-if (ENV == "test") {
-    db = new Pool({
-        user: POSTGRES_USER,
-        host: POSTGRES_HOST,
-        password: POSTGRES_PASSWORD,
-        database: POSTGRES_DB_TEST,
-    });
-} else {
-    db = new Pool({
-        user: POSTGRES_USER,
-        host: POSTGRES_HOST,
-        password: POSTGRES_PASSWORD,
-        database: POSTGRES_DB_DEV,
-    });
+try {
+    if (ENV == "test") {
+        db = new Pool({
+            user: POSTGRES_USER,
+            host: POSTGRES_HOST,
+            password: POSTGRES_PASSWORD,
+            database: POSTGRES_DB_TEST,
+        });
+    } else {
+        db = new Pool({
+            user: POSTGRES_USER,
+            host: POSTGRES_HOST,
+            password: POSTGRES_PASSWORD,
+            database: POSTGRES_DEV_DB,
+        });
+    }
+} catch (error) {
+    throw new Error("failed to connect to database");
 }
-export default db;
+export default db as Pool;
