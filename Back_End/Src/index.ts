978fca7d4a2c2daa6675f0343fcbb82500e4b731
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-23 22:12:43 +0200

    Updated_Index.ts

--- Back_End/Src/index.ts
+++ Back_End/Src/index.ts
@@ -2,25 +2,17 @@ import express, { Request, Response } from "express";
 import routes from "./routes/index.routes";
 import errorMiddleware from "./middleware/error.middleware";
 import dotenv from "dotenv";
-import db from "./database";
 
 dotenv.config();
 const { SERVER_PORT } = process.env;
 
 const app = express();
+app.use(express.json());
 app.use("/api", routes);
 
 app.get("/", (req: Request, res: Response) => {
-    throw new Error();
+    res.send("HELLO WORLD !!");
 });
-const test = async () => {
-    const connection = await db.connect();
-    const sql = "select * from users";
-    const result = await connection.query(sql);
-    console.log(result.rows);
-    connection.release();
-};
-test();
 
 app.use(errorMiddleware);
 app.listen(SERVER_PORT, () => {
