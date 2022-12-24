6fd082215140e7726b93d6e3d1fa9236779d1be2
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-24 21:49:18 +0200

    updated_Products_Model

--- Back_End/Src/models/product-model.ts
+++ Back_End/Src/models/product-model.ts
@@ -32,4 +32,54 @@ export class User {
         }
     }
 
+    // CREATE A USER
+    async create(u: UserType): Promise<UserType> {
+        try {
+            const connection = await db.connect();
+            const sql = `INSERT INTO products (email,username,password) VALUES ($1,$2,$3) RETURNING *;`;
+            const hash = bcrypt.hashSync(
+                u.password + BCRYPT_PASSWORD,
+                parseInt(SALT_ROUND as string)
+            );
+            const query = await connection.query(sql, [
+                u.email,
+                u.username,
+                hash,
+            ]);
+            connection.release();
+            return query.rows[0];
+        } catch (error) {
+            throw new Error("Cant Retreve Data From Table Users");
+        }
+    }
 
+    //UPDATE A USER
+    async update(u: UserType): Promise<UserType> {
+        try {
+            const connection = await db.connect();
+            const sql = `UPDATE users SET email = ($1),username=($2) , password =($3) RETURNING *;`;
+            const query = await connection.query(sql, [
+                u.email,
+                u.username,
+                u.password,
+            ]);
+            connection.release();
+            return query.rows[0];
+        } catch (error) {
+            throw new Error("Cant Retreve Data From Table Users");
+        }
+    }
+
+    // DELETE A USER
+    async delete(id: number): Promise<UserType> {
+        try {
+            const connection = await db.connect();
+            const sql = `DELETE FROM products WHERE id = ($1) RETURNING *`;
+            const query = await connection.query(sql, [id]);
+            connection.release();
+            return query.rows[0];
+        } catch (error) {
+            throw new Error("Cant Retreve Data From Table Users");
+        }
+    }
+}
